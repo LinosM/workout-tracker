ab7e3a1b3c5b26425cf844a5f92446a789ea22b8
LinosM <Zemnas768@gmail.com>
2021-04-06 17:04:03 -0400

    removed unused modules, simplified app.get routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,15 +1,7 @@
 const path = require('path');
-const express = require("express");
-const router = express.Router();
 
 module.exports = (app) => {
-    app.get("/", (req, res) => {
-        res.sendFile(path.join(__dirname, "../public/index.html"));
-    });
-    app.get("/exercise", (req, res) => {
-        res.sendFile(path.join(__dirname, "../public/exercise.html"));
-    });
-    app.get("/stats", (req, res) => {
-        res.sendFile(path.join(__dirname, "../public/stats.html"));
-    });
+    app.get("/", (req, res) => res.sendFile(path.join(__dirname, "../public/index.html")));
+    app.get("/exercise", (req, res) => res.sendFile(path.join(__dirname, "../public/exercise.html")));
+    app.get("/stats", (req, res) => res.sendFile(path.join(__dirname, "../public/stats.html")));
 };
