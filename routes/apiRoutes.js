63d3d6b966f622169044a5a2c3d8b25d30042614
LinosM <Zemnas768@gmail.com>
2021-04-06 17:01:44 -0400

    added additional api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,15 +1,31 @@
 const Workout = require("../models/workout.js");
-const express = require("express");
-const router = express.Router();
 
-router.post('/api/workouts', (req, res) => {
-    Workout.find({})
-    .then(db => {
-        res.json(db);
-    })
-    .catch(err => {
-        res.status(400).json(err);
-    })
-});
+module.exports = (app) => {
+    app.get('/api/workouts', (req, res) => {
+        Workout.find({})
+        .then(db => res.json(db))
+        .catch(err => res.status(400).json(err))
+    });
 
-module.exports = router;
+    app.post('/api/workouts', (req, res) => {
+        Workout.create({})
+        .then(db => res.json(db))
+        .catch(err => res.status(400).json(err))
+    });
+
+    app.put("/api/workouts/:id", ({ body, params }, res) => {   
+        Workout.findByIdAndUpdate(  
+            params.id,
+            { $push:{exercises:body} },
+            { new: true, runValidators:true }
+        )
+        .then(db => res.json(db))
+        .catch(err => res.status(400).json(err))
+    });
+
+    app.get('/api/workouts/range', (req, res) => {
+        Workout.find({})
+        .then(db => res.json(db))
+        .catch(err => res.status(400).json(err))
+    });
+}
