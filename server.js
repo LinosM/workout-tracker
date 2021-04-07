b24b76f3773376ff310d14895636d838e8e7f4a3
LinosM <zemnas768@gmail.com>
2021-04-07 16:24:08 -0400

    added compression

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const logger = require("morgan");
+const compression = require('compression');
 
 const PORT = process.env.PORT || 3000;
 
@@ -11,7 +12,7 @@ app.use(express.json());
 app.use(logger("dev"));
 app.use(express.static("public"));
 app.use(express.static(__dirname));
-
+app.use(compression());
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
